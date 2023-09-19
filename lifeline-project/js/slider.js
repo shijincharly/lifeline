f55fb21eeab982197c29f1aec2feaadffdff74e2
shijincharly <128212461+shijincharly@users.noreply.github.com>
2023-09-20 00:34:00 +0530

    Delete lifeline-project directory

--- lifeline-project/js/slider.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.slider').slick({
-        autoplay: true,
-        autoplaySpeed: 3000, // Change slide every 3 seconds
-        dots: true, // Display navigation dots
-        arrows: false // Hide the default navigation arrows
-    });
-});
